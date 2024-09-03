d18e9e428210337c9545de002cc0449df74fa753
RiccardoMacchi <rick.mac97@outlook.com>
2024-09-03 15:32:42 +0200

    created methods to determine discount based on period of the year

--- Models/Movie.php
+++ Models/Movie.php
@@ -3,11 +3,32 @@
 class Movie {
     public $name;
     public $author;
-    public $relaseDate;
+    public $relase_date;
     public $price;
+    public $discount_period;
 
-    function __construct($_name,$_author){
+    function __construct($_name,$_author,$_price){
         $this->name = $_name;
         $this->author = $_author;
+        $this->price = $_price;
+    }
+
+    public function discountOnPeriod(){
+        $actual_month = 8;
+        if($actual_month >= 6 && $actual_month <= 8){
+            $discount = 0.10;
+        } elseif($actual_month === 11 || $actual_month === 12 || $actual_month === 1 || $actual_month === 2) {
+            $discount = 0.20;
+        } else {
+            $discount = 0;
+        }
+        return $discount;
+        
+    }
+
+    public function finalPrice(){
+        $discount = $this->discountOnPeriod();
+        $final_price = $this->price * (1 - $discount);
+        echo number_format($final_price,2,',','.');
     }
 }
--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 
 require_once __DIR__ . '/Models/Movie.php';
 
-$spiderman = new Movie('Spiderman','Stan Lee');
+$spiderman = new Movie('Spiderman','Stan Lee', 14);
 
 // $spiderman->name = 'Spiderman';
 // $spiderman->author = 'Stan Lee';
-$spiderman->relaseDate = '2002-04-29';
-$spiderman->price = 14;
+$spiderman->relase_date = '2002-04-29';
+// $spiderman->price = 14;
 
 var_dump($spiderman);
+$spiderman->finalPrice();
